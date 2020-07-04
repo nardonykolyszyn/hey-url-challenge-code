4ccfc322fd06d29dc4fe518af1ee6c313e00de78
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-07-04 16:11:59 -0500

    create urls resource

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,48 +3,54 @@
 class UrlsController < ApplicationController
   def index
     @url = Url.new
-    @urls = [
-      Url.new(short_url: '123', original_url: 'http://google.com', created_at: Time.now),
-      Url.new(short_url: '456', original_url: 'http://facebook.com', created_at: Time.now),
-      Url.new(short_url: '789', original_url: 'http://yahoo.com', created_at: Time.now)
-    ]
+    @urls = Url.all.order(created_at: :desc)
   end
 
   def create
-    # create a new URL record
+    @url = Url.new(url_params)
+
+    if @url.save
+      flash[:notice] = 'Url saved successfully'
+    else
+      flash[:error] = @url.errors.full_message
+    end
+
+    redirect_to root_path
   end
 
   def show
-    @url = Url.new(short_url: '123', original_url: 'http://google.com', created_at: Time.now)
-    # implement queries
-    @daily_clicks = [
-      ['1', 13],
-      ['2', 2],
-      ['3', 1],
-      ['4', 7],
-      ['5', 20],
-      ['6', 18],
-      ['7', 10],
-      ['8', 20],
-      ['9', 15],
-      ['10', 5]
-    ]
-    @browsers_clicks = [
-      ['IE', 13],
-      ['Firefox', 22],
-      ['Chrome', 17],
-      ['Safari', 7]
-    ]
-    @platform_clicks = [
-      ['Windows', 13],
-      ['macOS', 22],
-      ['Ubuntu', 17],
-      ['Other', 7]
-    ]
+    @daily_clicks = url.newest_clicks.group_by do |c|
+      c.created_at.strftime('%d')
+    end.transform_values(&:count).to_a
+
+    @browsers_clicks = url.newest_clicks.group_by do |c|
+      c.browser
+    end.transform_values(&:count).to_a
+
+    @platform_clicks = url.newest_clicks.group_by do |c|
+      c.platform
+    end.transform_values(&:count).to_a
   end
 
   def visit
-    # params[:url]
-    # @url = find url
+    browser = Browser.new(request.env['HTTP_USER_AGENT'])
+
+    if url.persisted?
+      new_click = url.register_click(browser)
+      url.register_counter_click if new_click
+
+      redirect_to url.original_url, status: :moved_permanently
+    end
+  end
+
+  private
+
+  def url
+    @url = Url.find_by(short_url: params[:url])
+  end
+
+  def url_params
+    params.require(:url)
+          .permit(:original_url)
   end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,54 @@
 # frozen_string_literal: true
 
 class Url < ApplicationRecord
-  # scope :latest, -> {}
+  # Callback
+  after_initialize :generate_original_url_scheme
+  around_create    :generate_short_url
+
+  validates :original_url, presence: true, format: {
+    with: /\A[\:\/\/'a-zA-Z0-9'\.]*\z/,
+    message: 'original_url has non-valid characters or any scheme'
+  }
+  # Associations
+  has_many :clicks
+
+  # Scopes
+  def newest_clicks
+    clicks.where('created_at >= ? AND created_at <= ?', Time.zone.now.beginning_of_month, Time.zone.now.end_of_month)
+  end
+
+  # It force HTTP protocol in case it is blank (https || http)
+  def shortened_url
+    Shortener.scheme_builder(original_url).build(
+      host: ENV.fetch('APP_HOST') { 'localhost' },
+      path: "/#{short_url}",
+      port: ENV['APP_PORT']
+    ).to_s
+  end
+
+  def register_counter_click
+    self.class.increment_counter(:clicks_count, id)
+  end
+
+  def register_click(browser)
+    clicks.build(
+      browser: browser.name,
+      platform: browser.platform.id
+    ).save
+  end
+
+  private
+
+  def generate_original_url_scheme
+    self.original_url = Shortener.clean_url(original_url) if original_url
+  end
+
+  def generate_short_url
+    self.short_url = loop do
+      random_token = Shortener.generate_short_option
+
+      break random_token unless self.class.exists?(short_url: random_token)
+    end
+    yield
+  end
 end
