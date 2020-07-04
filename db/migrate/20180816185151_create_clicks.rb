85d66d4df14d19f6fa4c6c012563c89cf5d361f3
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-07-04 16:12:21 -0500

    fix migrations

--- db/migrate/20180816185151_create_clicks.rb
+++ db/migrate/20180816185151_create_clicks.rb
@@ -4,7 +4,7 @@ class CreateClicks < ActiveRecord::Migration[5.2]
   def change
     create_table :clicks do |t|
       t.timestamps
-      t.references :url, foreign_key: true
+      t.references :url, foreign_key: { on_delete: :cascade }, index: true
       t.string :browser, null: false
       t.string :platform, null: false
     end
