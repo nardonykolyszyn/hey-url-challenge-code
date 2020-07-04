86dfae2b6fad07bc135472caf8cc8a37fa86b6ce
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2020-07-04 16:12:32 -0500

    add missing stats route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   root to: 'urls#index'
 
-  resources :urls, only: %i[index create show], param: :url
   get ':url', to: 'urls#visit', as: :visit
+
+  resources :urls, only: %i[index create], param: :url do
+    get :stats, to: 'urls#show', on: :member
+  end
 end
